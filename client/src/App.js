22f4e5329f7dcec75134333b35867d7ff174787f
Trishane <trishaneww@gmail.com>
2023-11-16 11:33:06 -0500

    created bargraph mockup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
-import Test from './components/Test';
+import Homepage from './components/homepage/Homepage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Viewdeck from './components/Viewdeck'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Settings from './components/settings/Settings'
 
 function App() {
   return (
@@ -8,7 +12,11 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Test />}/>
+          <Route path='/' element={<Homepage />}/>
+          <Route path='/settings' element={<Settings />}/>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/deck/:id' element={<Viewdeck />} />
         </Routes>
       </div>  
     </BrowserRouter>
